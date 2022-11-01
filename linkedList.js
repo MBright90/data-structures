b4830a45db68ee2e29d876afd6dba6fd7e2434af
mbright90 <michael.bright90@icloud.com>
2022-11-01 11:50:30 +0000

    Write recursive functions for basic list operations

--- linkedList.js
+++ linkedList.js
@@ -1,60 +1,77 @@
 import Node from "./listNode"
 
-class List {
+export default class List {
     constructor() {
-        this.head = null
-    }
-
-    // Add node containing new value to the end of the list (new head)
-    append(value) {
-        let head
-        const newNode = Node(value)
-        if (this.head === null) this.head = newNode
+        this.listHead = null
     }
 
     // Add node containing new value to the start of the list (new head)
-    prepend(value) {
-
+    prependBase(value) {
+        const nextNode = this.listHead;
+        this.listHead = Node(value)
+        this.listHead.pointer = nextNode;
     }
 
-    // Return total size of the list
-    size() {
+    // Add node containing new value to the end of the list (new head)
+    appendBase(value, currentNode = this.listHead) {
+        if (currentNode.pointer === null) {
+            const newNode = Node.value;
+            currentNode.pointer = newNode;
+        }
+        this.append(value, currentNode.pointer)
+    }
 
+    // Return total length of the list
+    sizeBase(count = 0, currentNode = this.listHead) {
+        if (currentNode.pointer = null) return count;
+        count++
+        return this.size(count, currentNode.pointer)
     }
 
     // Return the first node within the list
-    head() {
-
+    headBase() {
+        return this.listHead
     }
 
     // Return the last node within the list
-    tail() {
-
+    tailBase(currentNode = this.listHead) {
+        if (currentNode.pointer === null) return currentNode;
+        return this.tail(currentNode.pointer)
     }
 
-    // Return the node at the given index
-    at(index){
-
+    // Return the node at the given index or -1 if index not within range
+    atBase(index, currentIndex = 0, currentNode = this.listHead) {
+        if (currentIndex === index) return currentNode;
+        if (currentNode = null) return -1;
+        currentIndex += 1;
+        return this.at(index, currentIndex, currentNode.pointer);
     }
 
     // Remove the last element in the list
-    pop() {
-
+    popBase(currentNode = this.listHead, previousNode = null) {
+        if (currentNode.pointer = null) previousNode.pointer = null;
+        this.pop(currentNode.pointer, currentNode);
     }
 
     // Returns true if the given value is within any node in the list or false if not found
-    contains(value) {
-
+    containsBase(value, currentNode = this.listHead) {
+        if (value === currentNode.value) return true;
+        if (currentNode.pointer === null) return false;
+        return this.contains();
     }
 
     // Returns the index of the given value if it is within the list or null if not found
-    find(value) {
+    findBase(value, index = 0) {
+        if (value === currentNode.value) return index;
+        if (currentNode.pointer === null) return null; 
 
     }
 
     // Returns a string representation of all values within the list
-    toString() {
-
+    toStringBase(string, currentNode = this.listHead) {
+        if (currentNode = null) return string += 'null'
+        string += `( ${currentNode.value} ) -> `
+        return this.toString(string, currentNode.pointer)
     }
 
     //#########################################//
@@ -62,12 +79,60 @@ class List {
     //#########################################//
 
     // Inserts a new node at a given index
-    insertAt(value, index) {
+    insertAt(value, index, currentIndex, currentNode = this.listHead) {
 
     }
 
     // Removes a node from a given index
-    removeAt(index) {
+    removeAt(index, currentIndex, currentNode = this.listHead) {
 
     }
+
+    //#########################################//
+    //########### RETURN FUNCTIONS ############//
+    //#########################################//
+
+    // Due to the recursive nature of my functions, I chose to return these functions as a 
+    // layer of security against the user passing in arguments to override the default arguments.
+
+    append(value) {
+        this.appendBase(value)
+    }
+
+    prepend(value) {
+        this.prependBase(value)
+    }
+
+    size() {
+        return this.sizeBase()
+    }
+
+    head() {
+        return this.headBase()
+    }
+
+    tail() {
+        return this.tailBase()
+    }
+
+    at(index) {
+        return this.atBase(index)
+    }
+
+    pop() {
+        this.popBase()
+    }
+
+    contains(value) {
+        this.containsBase(value)
+    }
+
+    find(value) {
+        return this.findBase(value)
+    }
+
+    tostring() {
+        return this.toStringBase()
+    }
+
 }
