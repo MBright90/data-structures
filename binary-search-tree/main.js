85d720fbe16efeadd89452bf1fda5aad44967fdd
mbright90 <michael.bright90@icloud.com>
2022-11-03 11:30:19 +0000

    Write function to pass in testing of binary search methods

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -1,4 +1,12 @@
 import BST from './binarySearchTree.js'
 
-const Tree = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+// function for testing passing of function as parameter to BST methods
+function printNode(node) {
+  console.log(`This node is ${node}`)
+}
+
+const Tree = new BST([1, 7, 4, 23, 8, 9, 6, 4, 3, 5, 7, 9, 67, 2, 6345, 324])
 Tree.prettyPrint()
+
+console.log(Tree.levelOrder()) // 7 3 23 1 5 8 324 2 4 6 9 67 6345 (correct level order structure)
+Tree.levelOrder(printNode)
