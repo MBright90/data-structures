ca1c8e49f5284826e69201caa2b6b18d7b367fe2
mbright90 <michael.bright90@icloud.com>
2022-11-03 12:00:55 +0000

    Add function calls to test tree traversal methods

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -1,12 +1,19 @@
 import BST from './binarySearchTree.js'
 
 // function for testing passing of function as parameter to BST methods
-function printNode(node) {
-  console.log(`This node is ${node}`)
+function squareNode(node) {
+  return node.value * node.value
 }
 
 const Tree = new BST([1, 7, 4, 23, 8, 9, 6, 4, 3, 5, 7, 9, 67, 2, 6345, 324])
 Tree.prettyPrint()
 
-console.log(Tree.levelOrder()) // 7 3 23 1 5 8 324 2 4 6 9 67 6345 (correct level order structure)
-Tree.levelOrder(printNode)
+console.log(Tree.levelOrder()) // [7, 3, 23, 1, 5, 8, 324, 2, 4, 6, 9, 67, 6345]
+// const outcomeObject = Tree.levelOrder(squareNode)
+// console.log(outcomeObject[4]) // {value: 5, outcome: 25}
+
+// console.log(Tree.inOrder()) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 67, 324, 6345]
+
+console.log(Tree.preOrder()) // [7, 3, 1, 2, 5, 4, 6, 23, 8, 9, 324, 67, 6345]
+
+console.log(Tree.postOrder()) // [2, 1, 4, 6, 5, 3, 9, 8, 67, 6345, 324, 23, 7]
