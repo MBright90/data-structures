f595e4507f651766e4566e7e1fbb9159dcf755aa
mbright90 <michael.bright90@icloud.com>
2022-11-03 12:59:07 +0000

    Add console tests for height and depth tree functions

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -9,11 +9,18 @@ const Tree = new BST([1, 7, 4, 23, 8, 9, 6, 4, 3, 5, 7, 9, 67, 2, 6345, 324])
 Tree.prettyPrint()
 
 console.log(Tree.levelOrder()) // [7, 3, 23, 1, 5, 8, 324, 2, 4, 6, 9, 67, 6345]
-// const outcomeObject = Tree.levelOrder(squareNode)
-// console.log(outcomeObject[4]) // {value: 5, outcome: 25}
+const outcomeObject = Tree.levelOrder(squareNode)
+console.log(outcomeObject[4]) // {value: 5, outcome: 25}
 
-// console.log(Tree.inOrder()) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 67, 324, 6345]
+console.log(Tree.inOrder()) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 67, 324, 6345]
 
 console.log(Tree.preOrder()) // [7, 3, 1, 2, 5, 4, 6, 23, 8, 9, 324, 67, 6345]
 
 console.log(Tree.postOrder()) // [2, 1, 4, 6, 5, 3, 9, 8, 67, 6345, 324, 23, 7]
+
+console.log(Tree.height(23)) // 3
+console.log(Tree.height(877)) // -1
+
+console.log(Tree.depth(3)) // 2
+console.log(Tree.depth(4)) // 4
+console.log(Tree.depth(99)) // -1
